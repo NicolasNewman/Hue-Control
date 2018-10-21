b2b25e0486abe6c099cd48549213538d125525fd
Nicolas Newman <nnewman316@gmail.com>
2018-10-21 15:45:20 -0500

    Implemented custom scrollbar and created card template

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -10,7 +10,7 @@ const host     = store.get('ip'),
 const elements = {
     mode_lights: document.querySelector('#mode--lights'),
     mode_groups: document.querySelector('#mode--groups'),
-    section_lights: document.querySelector('.section__lights')
+    section_lights: document.querySelector('.section__lights').firstElementChild
 }
 
 const homeController = (() => {
@@ -27,14 +27,50 @@ const homeController = (() => {
             elements.mode_lights.style.color = '#fffb28';
             elements.mode_groups.style.color = '#fff';
             
-            hue.lights().then(displayResult).done();
+            hue.lights((err, lights) => {
+                if (err) throw err;
+                while(elements.section_lights.firstChild) elements.section_lights.removeChild(elements.section_lights.firstChild);
+                lights.lights.forEach((light) => {
+                    console.log(light);
+                    elements.section_lights.insertAdjacentHTML('beforeend', `
+                        <div data-id="${light.id}" class="light__card">
+                            <p>${light.type}</p>
+                            <p>${light.name}</p>
+                        </div>                        
+                    `);
+                })
+            });
         });
         elements.mode_groups.addEventListener('click', (e) => {
             elements.mode_groups.style.color = '#fffb28';
-            elements.mode_lights.style.color = '#fff';            
+            elements.mode_lights.style.color = '#fff';    
+            
+            hue.groups((err, groups) => {
+                if (err) throw err;
+                console.log(groups);
+                while(elements.section_lights.firstChild) elements.section_lights.removeChild(elements.section_lights.firstChild);
+                groups.forEach((group) => {
+                    if (group.id !== "0") {
+                        elements.section_lights.insertAdjacentHTML('beforeend', `
+                            <div data-id="${group.id}" class="light__card">
+                                <p>${group.name}</p>
+                            </div>                        
+                        `);
+                    }
+                });
+                // groups.groups.forEach((light) => {
+                //     console.log(light);
+                //     elements.section_lights.insertAdjacentHTML('beforeend', `
+                //         <div data-id="${light.id}" class="light__card">
+                //             <p>${light.type}</p>
+                //             <p>${light.name}</p>
+                //         </div>                        
+                //     `);
+                // })
+            });
         });
     }
-
+    
     return {
         init: () => {
             initEventListener();
