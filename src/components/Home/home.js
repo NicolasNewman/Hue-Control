d65c7948c300c1ccbaaf922f8124f91be73b8dc4
Nicolas Newman <nnewman316@gmail.com>
2018-10-20 19:35:27 -0500

    Started development of the home window

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,11 +1,16 @@
 const Hue    = require('node-hue-api'),
       HueApi = Hue.HueApi,
-      hue    = new HueApi(),
       Store  = require('electron-store'),
       store  = new Store();
 
-const elements = {
+const host     = store.get('ip'),
+      username = store.get('username'),
+      hue      = new HueApi(host, username);
 
+const elements = {
+    mode_lights: document.querySelector('#mode--lights'),
+    mode_groups: document.querySelector('#mode--groups'),
+    section_lights: document.querySelector('.section__lights')
 }
 
 const homeController = (() => {
@@ -13,13 +18,26 @@ const homeController = (() => {
         console.log(result);
     };
     const init = () => {
-        hue.lights().then(displayResult).done();
         hue.groups().then(displayResult).done();
         hue.scenes().then(displayResult).done();        
     };
+
+    const initEventListener = () => {
+        elements.mode_lights.addEventListener('click', (e) => {
+            elements.mode_lights.style.color = '#fffb28';
+            elements.mode_groups.style.color = '#fff';
+            
+            hue.lights().then(displayResult).done();
+        });
+        elements.mode_groups.addEventListener('click', (e) => {
+            elements.mode_groups.style.color = '#fffb28';
+            elements.mode_lights.style.color = '#fff';            
+        });
+    }
+
     return {
         init: () => {
-            init();
+            initEventListener();
         }
     }
 })();
